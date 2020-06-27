ef456a45498eace80118ee13e7d6b990f4ac5617
Ryan Murphy <rdmurphy@users.noreply.github.com>
2020-06-26 17:10:56 -0700

    A bit more time

--- test/test.js
+++ test/test.js
@@ -7,6 +7,8 @@ const { chromium, webkit, firefox } = require('playwright');
 const browserName = process.env.BROWSER || 'chromium';
 
 describe('browser tests', function () {
+	this.timeout(2e4);
+
 	let browser;
 	let page;
 
