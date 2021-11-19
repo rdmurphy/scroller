65fa4b4f02c242676f6fe526ac21ca02a4b16b6c
Ryan Murphy <rdmurphy@users.noreply.github.com>
2021-11-19 01:16:08 -0800

    Works-ish

---
+++ test/helpers.js
@@ -0,0 +1,41 @@
+export function scrollToTopOfElement(
+	/** @type {import('playwright-core').Locator} */ locator,
+) {
+	return locator.evaluate((el) => {
+		window.scrollTo(
+			0,
+			el.getBoundingClientRect().top +
+				10 +
+				window.scrollY -
+				window.innerHeight / 2,
+		);
+	});
+}
+
+export function scrollAboveElement(
+	/** @type {import('playwright-core').Locator} */ locator,
+) {
+	return locator.evaluate((el) => {
+		window.scrollTo(
+			0,
+			el.getBoundingClientRect().top -
+				1 +
+				window.scrollY -
+				window.innerHeight / 2,
+		);
+	});
+}
+
+export function scrollBelowElement(
+	/** @type {import('playwright-core').Locator} */ locator,
+) {
+	return locator.evaluate((el) => {
+		window.scrollTo(
+			0,
+			el.getBoundingClientRect().bottom +
+				1 +
+				window.scrollY -
+				window.innerHeight / 2,
+		);
+	});
+}
--- test/test.spec.js
+++ test/test.spec.js
@@ -1,6 +1,12 @@
 import { test, expect } from '@playwright/test';
+import {
+	scrollAboveElement,
+	scrollBelowElement,
+	scrollToTopOfElement,
+} from './helpers.js';
 
-test('basic test', async ({ page }) => {
+test('should do basic triggering', async ({ page }) => {
+	// visit our page
 	await page.goto('/test/fixtures/');
 
 	// expect the title to be set
@@ -16,44 +22,58 @@ test('basic test', async ({ page }) => {
 	const firstScene = scenes.first();
 
 	// make sure the first scene isn't active
-	await expect(
-		await firstScene.evaluate(
-			(el) => window.getComputedStyle(el).backgroundColor,
-		),
-	).toBe('rgb(255, 255, 224)');
+	await expect(firstScene).not.toHaveClass(/active/);
 
 	// scroll to the active point for the first scene
-	await firstScene.evaluate((el) => {
-		window.scrollTo(
-			0,
-			el.getBoundingClientRect().top + window.scrollY - window.innerHeight / 2,
-		);
-	});
-	await page.waitForTimeout(100);
+	await scrollToTopOfElement(firstScene);
 
 	// make sure it is active
-	await expect(
-		await firstScene.evaluate(
-			(el) => window.getComputedStyle(el).backgroundColor,
-		),
-	).toBe('rgb(175, 238, 238)');
+	await expect(firstScene).toHaveClass(/active/);
 
 	// scroll to just after the first scene
-	await firstScene.evaluate((el) => {
-		window.scrollTo(
-			0,
-			el.getBoundingClientRect().bottom +
-				1 +
-				window.scrollY -
-				window.innerHeight / 2,
-		);
-	});
-	await page.waitForTimeout(100);
+	await scrollBelowElement(firstScene);
 
 	// make sure it is no longer active
-	await expect(
-		await firstScene.evaluate(
-			(el) => window.getComputedStyle(el).backgroundColor,
-		),
-	).toBe('rgb(255, 255, 224)');
+	await expect(firstScene).not.toHaveClass(/active/);
+});
+
+test('should allow for adding and removing listeners', async ({ page }) => {
+	// visit our page
+	await page.goto('/test/fixtures/bare/');
+
+	// expect the title to be set
+	await expect(page).toHaveTitle(/@newswire\/scroller/);
+
+	// grab the first scene
+	const allScenes = page.locator('.scene');
+	const scene = allScenes.first();
+
+	// confirm scrolling doesn't yet activate anything
+	await scrollToTopOfElement(scene);
+	await expect(scene).not.toHaveClass(/active/);
+	await scrollAboveElement(scene);
+
+	// get the scroller handle
+	const scroller = await page.evaluateHandle('window.scroller');
+
+	const removeHandler = await scroller.evaluateHandle((scroller) => {
+		return scroller.on('enter', (d) => {
+			d.element.classList.add('active');
+		});
+	});
+	await page.waitForTimeout(15);
+
+	// confirm scrolling now activates
+	await scrollToTopOfElement(scene);
+	await expect(scene).toHaveClass(/active/);
+	await scrollAboveElement(scene);
+
+	// remove the listener
+	removeHandler.evaluate((fn) => fn());
+	await page.waitForTimeout(15);
+
+	// now shouldn't activate again
+	await scrollToTopOfElement(scene);
+	await expect(scene).not.toHaveClass(/active/);
+	await scrollAboveElement(scene);
 });
